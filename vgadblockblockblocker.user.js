f925b3f13f8b3d06d7927a058034d8ec9b4c18e5
Christian Bryn <chr.bryn@gmail.com>
2015-02-06 02:11:25 +0100

    remove useless function return

--- vgadblockblockblocker.user.js
+++ vgadblockblockblocker.user.js
@@ -15,7 +15,7 @@ var script = document.createElement('script');
 
 // method 1:
 // override the function that inserts the unwanted ...content
-script.innerHTML = 'window.__AB__ = function() { return true; }';
+script.innerHTML = 'window.__AB__ = function() {}';
 
 // method 2:
 // probably better. probably disables the function that makes calls to the (german) adblock detector provider
